5e8fed8c60bae42058769d2d4a278037512b950e
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-18 16:41:20 +0100

    Remove uncessecary blueprint and make code drier

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
-from controllers.orders_controller import orders_blueprint, order_blueprint
+from controllers.orders_controller import orders_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(orders_blueprint)
-app.register_blueprint(order_blueprint)
 
 @app.route('/')
 def index():
--- controllers/orders_controller.py
+++ controllers/orders_controller.py
@@ -3,13 +3,14 @@ from models.order_list import orders
 
 orders_blueprint = Blueprint("orders", __name__)
 
-order_blueprint = Blueprint("order", __name__)
+
 
 @orders_blueprint.route("/orders")
 def index():
     return render_template("index.html", title="My Order List", order_list=orders)
 
-@order_blueprint.route("/orders/<index>")
+@orders_blueprint.route("/orders/<index>")
 def order(index):
-    return render_template("order.html", title=orders[int(index)].customer_name, order=orders[int(index)])
+    order = orders[int(index)]
+    return render_template("order.html", title=order.customer_name, order=order)
 
