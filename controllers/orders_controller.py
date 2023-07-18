ea9c890fce1b519a904f94fb0e2c79c9a09d62a3
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-18 16:16:22 +0100

    Link orders in list to temp view for order

--- controllers/orders_controller.py
+++ controllers/orders_controller.py
@@ -3,6 +3,8 @@ from models.order_list import orders
 
 orders_blueprint = Blueprint("orders", __name__)
 
+order_blueprint = Blueprint("order", __name__)
+
 @orders_blueprint.route("/orders")
 def index():
     return render_template("index.html", title="My Order List", order_list=orders)
@@ -10,3 +12,7 @@ def index():
 @orders_blueprint.route("/orders/<index>")
 def get_order(index):
     return f"{orders[int(index)].topping}" "<h1>This is the order youre looking for</h1>"
+
+@order_blueprint.route("/orders/<index>")
+def order(index):
+    return render_template("order.html", title=orders[index].customer_name, order=orders[index])
