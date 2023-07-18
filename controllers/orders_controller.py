aabb65ef9963f0f4d577dddfed966329860bf42e
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-18 15:46:44 +0100

    Add route which displays an orders topping from the index

--- controllers/orders_controller.py
+++ controllers/orders_controller.py
@@ -6,3 +6,7 @@ orders_blueprint = Blueprint("orders", __name__)
 @orders_blueprint.route("/orders")
 def index():
     return render_template("index.html", title="My Order List", order_list=orders)
+
+@orders_blueprint.route("/orders/<index>")
+def get_order(index):
+    return f"{orders[int(index)].topping}" "<h1>This is the order youre looking for</h1>"
