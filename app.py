9e1a11881329137e70c9c9941f05d7c791dba012
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-18 16:25:37 +0100

    Add order.html view which renders when an order within the list is clicked

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from controllers.orders_controller import orders_blueprint
+from controllers.orders_controller import orders_blueprint, order_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(orders_blueprint)
+app.register_blueprint(order_blueprint)
 
 @app.route('/')
 def index():
--- controllers/orders_controller.py
+++ controllers/orders_controller.py
@@ -9,10 +9,7 @@ order_blueprint = Blueprint("order", __name__)
 def index():
     return render_template("index.html", title="My Order List", order_list=orders)
 
-@orders_blueprint.route("/orders/<index>")
-def get_order(index):
-    return f"{orders[int(index)].topping}" "<h1>This is the order youre looking for</h1>"
-
 @order_blueprint.route("/orders/<index>")
 def order(index):
-    return render_template("order.html", title=orders[index].customer_name, order=orders[index])
+    return render_template("order.html", title=orders[int(index)].customer_name, order=orders[int(index)])
+
